17af142189038f4daa208409df1c9da858bf9931
Brinthan <brinthsega@gmail.com>
2020-12-23 23:59:05 +0530

    adding dbcontext

---
+++ BrieAPI/Persistence/BrieDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrieAPI.Persistence
+{
+    public class BrieDbContext : DbContext
+    {
+        public BrieDbContext(DbContextOptions<BrieDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
--- BrieAPI/Startup.cs
+++ BrieAPI/Startup.cs
@@ -1,7 +1,9 @@
+using BrieAPI.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,8 @@ namespace BrieAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BrieDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("Default")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
